3908d401a42abfe9fbe7256098362332cd3d8a16
Feet <59450804@qq.com>
2017-12-23 16:36:45 +0800

    添加项目文件。

---
+++ LTM.School/Core/Models/Course.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LTM.School.Application.EnumsType;
+
+namespace LTM.School.Core.Models
+{
+    public class Course
+    {
+		public int Id { get; set; }
+
+		public string Title { get; set; }
+
+		public int Credits { get; set; }
+		public CourseGrade Grade { get; set; }
+		public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
---
+++ LTM.School/Core/Models/Enrollment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LTM.School.Core.Models
+{
+    public class Enrollment
+    {
+		public int EnrollmentId { get; set; }
+		public int StudentId { get; set; }
+		public int CourseId { get; set; }
+    }
+}
